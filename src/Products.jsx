66a8933f1f3beac07e1ff4377b4e8ef34e699f49
waleed <waleeddweikat67@gmail.com>
2023-10-23 19:16:40 +0300

    first commit

---
+++ src/Products.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export class Products extends Component {
+  render() {
+    return (
+      <>
+      <div className="products">
+      <div className="product">
+        <h2>Lorem, ipsum.</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="product">
+        <h2>Lorem, ipsum.</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="product">
+        <h2>Lorem, ipsum.</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      </div>
+      </>
+    )
+  }
+}
+
+export default Products
